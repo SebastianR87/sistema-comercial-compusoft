9936b2a034b197109d6fc8cf0784d8f2ba8ba610
Sebastian Rondo <sebastian.j.rondo@gmail.com>
2026-05-07 14:00:19 -0500

    Creacion del proyecto: Sistema-comercial-Compusoft

---
+++ src/main/java/com/compusoft/sistema/SistemaComercialCompusoft.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.compusoft.sistema;
+
+/**
+ *
+ * @author sebas
+ */
+public class SistemaComercialCompusoft {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
